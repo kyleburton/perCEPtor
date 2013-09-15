7712e1e43815ad013462040ff901e51f33fc98a4
Kyle Burton <kyle.burton@gmail.com>
2013-09-15 16:05:01 -0400

    use latest tep

--- examples/web-stats/project.clj
+++ examples/web-stats/project.clj
@@ -6,5 +6,5 @@
   :main web-stats.main
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.relaynetwork/perceptor "1.0.0-SNAPSHOT"]
-                 [com.github.kyleburton/teporingo "1.0.1-SNAPSHOT"]
+                 [com.github.kyleburton/teporingo "2.1.16"]
                  [noir "1.1.0"]])
